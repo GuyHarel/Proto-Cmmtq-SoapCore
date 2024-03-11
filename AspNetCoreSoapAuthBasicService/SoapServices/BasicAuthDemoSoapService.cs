75e240b5188d2ade5ea6a5e1221c17469feec658
GuyHarel <guyharel13@gmail.com>
2024-03-11 15:19:46 -0400

    log du service soap avec trace et json de la requête

--- AspNetCoreSoapAuthBasicService/SoapServices/BasicAuthDemoSoapService.cs
+++ AspNetCoreSoapAuthBasicService/SoapServices/BasicAuthDemoSoapService.cs
@@ -1,13 +1,22 @@
 ﻿using Microsoft.AspNetCore.Authorization;
+using System.Text.Json;
 
 namespace TestProject.SoapServices
 {
 
+
     public class BasicAuthDemoSoapService : IBasicAuthDemoSoapService
     {
+        private ILogger<BasicAuthDemoSoapService> _logger;
 
+        public BasicAuthDemoSoapService(ILogger<BasicAuthDemoSoapService> logger)
+        {
+            _logger = logger;
+        }
         public DemoSoapReponse ObtenirDemoSoapReponse(DemoSoapRequest request)
         {
+            _logger.LogTrace("ObtenirDemoSoapReponse {request}", JsonSerializer.Serialize<DemoSoapRequest>(request));
+
             var resultat = new DemoSoapReponse
             { 
                 CodErreur = "0",
